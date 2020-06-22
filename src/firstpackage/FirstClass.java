e2b473230383b530ee7ea6f0e15d46d5109568db
zeynpclk <67173249+zeynpclk@users.noreply.github.com>
2020-06-23 02:49:50 +0300

    Update FirstClass.java
    artik cozum ariyoruz

--- src/firstpackage/FirstClass.java
+++ src/firstpackage/FirstClass.java
@@ -9,7 +9,7 @@ public class FirstClass {
 
 		
 
-		System.out.println("conflict yapmak istiyorum");
+		System.out.println("we are ageinst the conflict");
 
 		System.out.println("first commit from remot repository");
 	}
